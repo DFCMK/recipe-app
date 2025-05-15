002b93c19b7c38079280ee1f190a96894591ea28
Denes <denes@pop-os.home>
2025-05-15 14:44:13 +0100

    Added remaining files from GFG repo because given API URLs where not accessable

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,21 @@
-import { getRecipeCard  } from "./getRecipeCard";
-
+import { getRecipeCard } from "./getRecipeCard.js";
+import { getCuisineCard } from "./getCuisine.js";
 
 const cardParentContainer = document.querySelector(".main");
+const cuisineParentContainer = document.querySelector(".cuisine-filter");
+const searchBox = document.querySelector(".input");
 
-const URL = "https://rapidapi.com/spoonacular/api/recipe-food-nutrition";
+const RECIPEURL = "https://recipeapi.prakashsakari.repl.co/api/recipes";
+const CUISINEURL =
+  "https://recipeapi.prakashsakari.repl.co/api/recipes/cuisines";
+
+let searchValue = "";
+let filteredArrOfRecipes = [];
+let arrOfSelectedCuisine = [];
 
 const createElement = (element) => document.createElement(element);
 
-const getRecipes = async (URL) => {
+const getData = async (URL) => {
   try {
     const { data } = await axios.get(URL);
     return data;
@@ -16,7 +24,63 @@ const getRecipes = async (URL) => {
   }
 };
 
-const recipes = await getRecipes(URL);
+const recipes = await getData(RECIPEURL);
+const cuisines = await getData(CUISINEURL);
+
+const getFilteredData = () => {
+  filteredArrOfRecipes =
+    searchValue?.length > 0
+      ? recipes.filter((recipe) =>
+          recipe.TranslatedRecipeName.toLowerCase().includes(searchValue)
+        )
+      : recipes;
+  if (arrOfSelectedCuisine?.length > 0) {
+    filteredArrOfRecipes =
+      searchValue?.length < 1 ? recipes : filteredArrOfRecipes;
+    filteredArrOfRecipes = filteredArrOfRecipes.filter((recipe) =>
+      arrOfSelectedCuisine.includes(recipe.Cuisine)
+    );
+  }
+  return filteredArrOfRecipes;
+};
+
+const searchInputHandler = (event) => {
+  searchValue = event.target.value.toLowerCase();
+  const filteredData = getFilteredData();
+  cardParentContainer.innerHTML = "";
+  getRecipeCard(filteredData, cardParentContainer, createElement);
+};
+
+const handleCusineClick = (event) => {
+  const id = event.target.dataset.id;
+  const isSelected = event.target.checked;
+  const selectedCuisine = cuisines.reduce(
+    (acc, cur) => (cur.ID === acc ? cur.Cuisine : acc),
+    id
+  );
+  arrOfSelectedCuisine = isSelected
+    ? [...arrOfSelectedCuisine, selectedCuisine]
+    : arrOfSelectedCuisine.filter((cuisine) => cuisine !== selectedCuisine);
+  const filteredArrOfCuisine = getFilteredData();
+  cardParentContainer.innerHTML = "";
+
+  getRecipeCard(filteredArrOfCuisine, cardParentContainer, createElement);
+};
+
+searchBox.addEventListener("keyup", searchInputHandler);
+
+cuisineParentContainer.addEventListener("click", handleCusineClick);
 
 getRecipeCard(recipes, cardParentContainer, createElement);
-console.log(recipes);
+getCuisineCard(cuisines, cuisineParentContainer, createElement);
+
+cardParentContainer.addEventListener("click", (event) => {
+  const cardId = event.target.dataset.id;
+  if (cardId) {
+    localStorage.clear();
+    localStorage.setItem("id", cardId);
+    location.href = "single-recipe.html";
+  }
+});
+
+console.log(cuisines);
